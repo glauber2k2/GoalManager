570446819275b3b4ed361e74f49e99cb6bc6786a
DevGlauber <devglaubermonteiro@gmail.com>
2024-09-13 15:57:16 -0300

    first commit

---
+++ src/db/seed.ts
@@ -0,0 +1,35 @@
+import { client, db } from '.'
+import { goalCompletions, goals } from './schema'
+import dayjs from 'dayjs'
+
+async function seed() {
+  await db.delete(goalCompletions)
+  await db.delete(goals)
+
+  const result = await db
+    .insert(goals)
+    .values([
+      {
+        title: 'Estudar',
+        desiredWeeklyFrequency: 3,
+      },
+      {
+        title: 'Academia',
+        desiredWeeklyFrequency: 4,
+      },
+    ])
+    .returning()
+
+  const startOfWeek = dayjs().startOf('week')
+
+  await db.insert(goalCompletions).values([
+    {
+      goalId: result[0].id,
+      createdAt: startOfWeek.toDate(),
+    },
+  ])
+}
+
+seed().finally(() => {
+  client.end()
+})
